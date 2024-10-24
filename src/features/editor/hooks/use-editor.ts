bcb9dee59c9de5efde81613a3f2f76388ca46784
alexander flores <alexfloresrayme@gmail.com>
2024-10-24 08:32:22 -0500

    feat: responsive canvas

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -1,14 +1,24 @@
-import { fabric } from "fabric"
-import { useCallback } from "react"
+import { fabric } from "fabric";
+import { useCallback, useState } from "react";
+
+import { useAutoResize } from "./use-auto-resize";
 
 export const useEditor = () => {
+  const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  useAutoResize({
+    canvas,
+    container
+  });
+
   const init = useCallback(
     ({
       initialCanvas,
       initialContainer
     }: {
-      initialCanvas: fabric.Canvas
-      initialContainer: HTMLDivElement
+      initialCanvas: fabric.Canvas;
+      initialContainer: HTMLDivElement;
     }) => {
       fabric.Object.prototype.set({
         cornerColor: "#FFF",
@@ -18,7 +28,7 @@ export const useEditor = () => {
         transparentCorners: false,
         borderOpacityWhenMoving: 1,
         cornerStrokeColor: "#3b82f6"
-      })
+      });
 
       const initialWorkspace = new fabric.Rect({
         width: 900,
@@ -31,26 +41,29 @@ export const useEditor = () => {
           color: "rgba(0,0,0,0.8)",
           blur: 5
         })
-      })
+      });
 
-      initialCanvas.setWidth(initialContainer.offsetWidth)
-      initialCanvas.setHeight(initialContainer.offsetHeight)
+      initialCanvas.setWidth(initialContainer.offsetWidth);
+      initialCanvas.setHeight(initialContainer.offsetHeight);
 
-      initialCanvas.add(initialWorkspace)
-      initialCanvas.centerObject(initialWorkspace)
-      initialCanvas.clipPath = initialWorkspace
+      initialCanvas.add(initialWorkspace);
+      initialCanvas.centerObject(initialWorkspace);
+      initialCanvas.clipPath = initialWorkspace;
+
+      setCanvas(initialCanvas);
+      setContainer(initialContainer);
 
       const test = new fabric.Rect({
         width: 300,
         height: 300,
-        name: "test",
         fill: "black"
-      })
+      });
 
-      initialCanvas.add(test)
+      initialCanvas.add(test);
+      initialCanvas.centerObject(test);
     },
     []
-  )
+  );
 
-  return { init }
-}
+  return { init };
+};
